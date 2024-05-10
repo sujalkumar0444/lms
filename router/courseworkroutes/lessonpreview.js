8bb7c7d918bf5e40d1ecf4379aa9b5e76be443e2
sujal kumar <sujalkumar0444@gmail.com>
2024-05-10 20:35:14 +0530

    updated

--- router/courseworkroutes/lessonpreview.js
+++ router/courseworkroutes/lessonpreview.js
@@ -1,35 +1,45 @@
 const router = require('express').Router();
-const { Lesson } = require("../../models/course_work");
+const { Lesson ,CourseProblem} = require("../../models/course_work");
 
 router.get('/:lessonid', async (req, res) => {
     try {
         let lessonid = req.params.lessonid;
-        let lesson = await Lesson.findOne({ _id: lessonid }).select('-_id -lesson_no -contentype').populate([
-            { 
-                path: 'problem_id', 
-                model: 'CourseProblem', 
-            }, 
-            { 
-                path: 'assessment_ref', 
-                model: 'Assessments', 
-                populate: { 
-                    path: 'problems.problem_ref', 
-                    model: 'CourseProblem',
-                } 
-            } 
-        ]);
+        let lesson = await Lesson.findOne({ _id: lessonid }).select('-_id -lesson_no -contentype');
+        if(!lesson)
+            {
+                return res.status(404).json({ message: 'Lesson not found' });
+            }
+        let result={};
+        result.lesson_title=lesson.lesson_title;
+        if(lesson.problem_id)
+            {
+             let problem = await CourseProblem.findOne({ _id: lesson.problem_id });
+             result.lesson_points=lesson.lesson_points;
+             result.problem_id={};
+             result.problem_id.problem_title=problem.problem_title;
+             result.problem_id.problem_description=problem.problem_description;
+             result.problem_id.sample_test_cases = problem.sample_test_cases.map(testCase => {
+                const { _id, ...rest } = testCase.toObject();
+                return rest;
+            });
+            
+            result.problem_id.hidden_test_cases = problem.hidden_test_cases.map(testCase => {
+                const { _id, ...rest } = testCase.toObject(); 
+                return rest;
+            });
+            
 
-        // Conditionally exclude lesson_points if its value is 0
-        if (lesson && lesson.lesson_points !== 0) {  
-            res.json(lesson);
-        } else if (lesson) {
-            // Omit lesson_points from lesson object
-            lesson = lesson.toObject();
-            delete lesson.lesson_points;
-            res.json(lesson);
-        } else {
-            res.status(404).json({ message: 'Lesson not found' });
-        }
+            }
+            else if(lesson.text_content)
+            {
+             result.text_content=lesson.text_content;
+            }
+            else{
+               return res.status(404).json({ message: 'Assessment cant be previewed' });
+            }
+
+            res.json(result);
+
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Internal server error' });
