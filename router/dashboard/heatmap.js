6b17b6518036ce9dff63584e5da9845a666e7aba
21r21a33333 <21r21a3333@mlrinstitutions.ac.in>
2024-06-27 23:19:10 +0530

    heapmap by rollno

--- router/dashboard/heatmap.js
+++ router/dashboard/heatmap.js
@@ -17,5 +17,20 @@ router.get("/",async (req, res) => {
         res.status(500).json({ success: false, error: error.message });
   }
 });
+router.get("/:rollno",async (req, res) => {
+    try
+    {
+        let roll_no = req.params.rollno;
+        // console.log(roll_no);
+        let dashboard_data=await dashboard.findOne({roll_no});
+        // console.log(dashboard_data);
+        res.send( {data : dashboard_data.daily_solved_problem_count});
+    }
+ 
+    catch (error) {
+        console.error("Error fetching daily problems count:", error);
+        res.status(500).json({ success: false, error: error.message });
+  }
+});
 
 module.exports = router;
