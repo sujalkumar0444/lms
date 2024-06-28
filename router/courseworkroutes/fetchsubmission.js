f1feeec72de5d9c357b7880f1af8559332c44595
21r21a33333 <21r21a3333@mlrinstitutions.ac.in>
2024-06-28 23:12:29 +0530

    minor changes in get submissions

--- router/courseworkroutes/fetchsubmission.js
+++ router/courseworkroutes/fetchsubmission.js
@@ -1,23 +1,24 @@
 const router = require('express').Router();
 const Submission = require("../../models/submissions");
 
-router.get('/:problem_id', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        const { problem_id} = req.params;
+        const { problem_id} = req.body;
         const roll_no = req.roll_no;
         const submission = await Submission.findOne({ problem_id });
-
-        if (!submission) {
+            
+        if (!(submission?.users?.length>0)) {
             return res.status(404).json({ message: 'No submissions found for this problem ID' });
         }
-
-        const user = submission.users.find(user => user.roll_no === roll_no);
-
-        if (!user) {
+        // console.log(submission)
+        console.log(submission.users)
+        console.log(roll_no)
+        const  usersubmission= submission.users.find(user => user.roll_no == roll_no); 
+        if (!usersubmission) {
             return res.status(404).json({ message: 'No submissions found for this user' });
         }
 
-        res.status(200).json({ submissions: user.submissions });
+        res.status(200).json({ submissions: usersubmission.submissions });
     } catch (error) {
         console.error(error);
         res.status(500).send("Internal server error");
