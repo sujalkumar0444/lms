576db056cc32aa6d20afd73662b9308160ea1f4f
21r21a33333 <21r21a3333@mlrinstitutions.ac.in>
2024-06-29 01:10:19 +0530

    changed fetch submissions to post

--- router/courseworkroutes/fetchsubmission.js
+++ router/courseworkroutes/fetchsubmission.js
@@ -1,21 +1,22 @@
 const router = require('express').Router();
 const Submission = require("../../models/submissions");
 
-router.get('/', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
         const { problem_id} = req.body;
+        console.log("problem_id"+problem_id)
         const roll_no = req.roll_no;
         const submission = await Submission.findOne({ problem_id });
             
-        if (!(submission?.users?.length>0)) {
-            return res.status(404).json({ message: 'No submissions found for this problem ID' });
+        if (!(submission?.users?.length > 0)) {
+            return res.status(401).json({ message: 'No submissions found for this problem ID' });
         }
         // console.log(submission)
         console.log(submission.users)
         console.log(roll_no)
         const  usersubmission= submission.users.find(user => user.roll_no == roll_no); 
         if (!usersubmission) {
-            return res.status(404).json({ message: 'No submissions found for this user' });
+            return res.status(401).json({ message: 'No submissions found for this user' });
         }
 
         res.status(200).json({ submissions: usersubmission.submissions });
